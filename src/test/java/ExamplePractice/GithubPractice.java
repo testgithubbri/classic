a9f5337fe1c08ea65e418ff37a819c22fb75767c
testgithubbri <subratkumarsahu402@gmail.com>
2022-07-25 13:10:46 +0530

    1

---
+++ src/test/java/ExamplePractice/GithubPractice.java
@@ -0,0 +1,11 @@
+package ExamplePractice;
+
+public class GithubPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("old monk");
+
+	}
+
+}
