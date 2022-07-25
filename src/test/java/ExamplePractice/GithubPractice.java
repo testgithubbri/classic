760c8b943d6d8057facb154b702a7aa4d1f8a514
subratk389 <103275237+testgithubbri@users.noreply.github.com>
2022-07-25 13:17:17 +0530

    Update GithubPractice.java

--- src/test/java/ExamplePractice/GithubPractice.java
+++ src/test/java/ExamplePractice/GithubPractice.java
@@ -5,7 +5,7 @@ public class GithubPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.err.println("old monk123");
-		System.out.println("okkk")
+		System.out.println("okkk");
 
 	}
 
